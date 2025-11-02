af4c53233a70c21df878c54293a22b11336e537a
Helder Oliveira <heldergfao@gmail.com>
2025-11-02 14:27:11 +0000

    Version 1.0.1: Added dependencies

--- plugin.video.osgatostv/main.py
+++ plugin.video.osgatostv/main.py
@@ -1,7 +1,13 @@
 # general imports
+import sys
+import os
+
+lib_path = os.path.join(os.path.dirname(__file__), 'lib')
+sys.path.insert(0, lib_path)
+
+# lib imports
 import requests
 from bs4 import BeautifulSoup
-import sys
 from urllib import parse
 import json
 
